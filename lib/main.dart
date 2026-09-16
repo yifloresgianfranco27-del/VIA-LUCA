cd69bfe48df0ebcfd3a57fb0103caff832332a04
Gianfrancoyiflores27 <yifloresgianfranco27@gmail.com>
2026-09-16 01:31:09 -0500

    Solucionar conflicto de nombres Path entre latlong2 y UI

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
-import 'package:latlong2/latlong.dart';
+import 'package:latlong2/latlong.dart' hide Path;
 import 'package:http/http.dart' as http;
 
 void main() {
